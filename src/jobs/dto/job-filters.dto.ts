5a7468cf3690ee8b136895f78236e392440d69b4
Pete <pete@manageable.works>
2024-05-07 21:38:12 +0100

    feat: allowing filtering on job location

--- src/jobs/dto/job-filters.dto.ts
+++ src/jobs/dto/job-filters.dto.ts
@@ -1,10 +1,25 @@
-import { Expose } from 'class-transformer';
-import { IsIn, IsOptional } from 'class-validator';
+import { Expose, Type } from 'class-transformer';
+import { IsIn, IsObject, IsOptional, ValidateNested } from 'class-validator';
 import { EMPLOYMENT_TYPES } from '../constants/employment-types';
+import { LOCATION_TYPES } from '../constants/location-types';
+
+class LocationFiltersDto {
+  @Expose()
+  @IsOptional()
+  @IsIn(LOCATION_TYPES)
+  type?: string;
+}
 
 export class JobFiltersDto {
   @Expose()
   @IsOptional()
   @IsIn(EMPLOYMENT_TYPES)
   employmentType?: string;
+
+  @Expose()
+  @IsOptional()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => LocationFiltersDto)
+  location?: LocationFiltersDto;
 }
