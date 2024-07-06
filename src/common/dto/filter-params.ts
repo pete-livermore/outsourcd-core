31ff54b2ed721091a7a5871d4dc8c10b87d63a33
Pete Livermore <peterlivermore@gmail.com>
2024-07-06 22:20:01 +1000

    refactor: add additional type to union

--- src/common/dto/filter-params.ts
+++ src/common/dto/filter-params.ts
@@ -4,5 +4,5 @@ import { IsOptional } from 'class-validator';
 export class FilterParams {
   @ApiPropertyOptional()
   @IsOptional()
-  filters?: { [key: string]: string | number | boolean };
+  filters?: { [key: string]: string | number | boolean | object };
 }
