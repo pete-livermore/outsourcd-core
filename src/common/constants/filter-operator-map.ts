10504421d02ddf6f861aa34696b42733568813ff
Pete Livermore <peterlivermore@gmail.com>
2024-07-08 18:42:04 +1000

    feat: add date operator map

--- src/common/constants/filter-operator-map.ts
+++ src/common/constants/filter-operator-map.ts
@@ -1,7 +1,19 @@
-export const FILTER_OPERATOR_MAP = {
+export const NUMERIC_OPERATOR_MAP = {
   gt: '>',
   lt: '<',
   gte: '>=',
   lte: '<=',
   eq: '=',
 } as const;
+
+export type NumericOperator = keyof typeof NUMERIC_OPERATOR_MAP;
+export const NUMERIC_OPERATORS = Object.keys(NUMERIC_OPERATOR_MAP);
+
+export const DATE_OPERATOR_MAP = {
+  after: '>',
+  before: '<',
+  at: '=',
+} as const;
+
+export type DateOperator = keyof typeof DATE_OPERATOR_MAP;
+export const DATE_OPERATORS = Object.keys(DATE_OPERATOR_MAP);
--- src/common/constants/filter-operators-params.ts
+++
@@ -1,3 +0,0 @@
-export const FILTER_OPERATOR_PARAMS = ['gt', 'lt', 'eq', 'gte', 'lte'] as const;
-
-export type FilterOperatorParam = (typeof FILTER_OPERATOR_PARAMS)[number];
