56f239342d08c59c0cbff1c6c4cd98f12ead8455
Pete Livermore <peterlivermore@gmail.com>
2024-07-08 18:47:00 +1000

    refactor: abstract logic into utility fn

--- src/common/interceptors/camel-to-snake.interceptor.ts
+++ src/common/interceptors/camel-to-snake.interceptor.ts
@@ -6,35 +6,14 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { camelToSnakeKeys } from '../utils/data/data-transformers';
 
 @Injectable()
 export class CamelCaseToSnakeCaseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((data) => {
-        function camelToSnake<T = any>(value: T) {
-          if (value === null || value === undefined) {
-            return value;
-          }
-          if (Array.isArray(value)) {
-            return value.map(camelToSnake);
-          }
-
-          if (typeof value === 'object' && !(value instanceof Date)) {
-            return Object.fromEntries(
-              Object.entries(value).map(([key, value]) => [
-                key
-                  .split(/(?=[A-Z])/)
-                  .join('_')
-                  .toLowerCase(),
-                camelToSnake(value),
-              ]),
-            );
-          }
-          return value;
-        }
-
-        return camelToSnake(data);
+        return camelToSnakeKeys(data);
       }),
     );
   }
