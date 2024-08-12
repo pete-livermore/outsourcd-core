52b2b88b42ba688582fe7968b34ac6da718406f9
Pete Livermore <peterlivermore@gmail.com>
2024-08-12 21:04:17 +1000

    refactor: use kysely postgres helpers for joined array

--- src/roles-permissions/repositories/roles.repository.ts
+++ src/roles-permissions/repositories/roles.repository.ts
@@ -1,13 +1,32 @@
-import { Database } from 'src/database/database';
+import { Database, Tables } from 'src/database/database';
 import { CreateRoleDto } from '../dto/create-role.dto';
 import { Role } from '../models/role.model';
 import { PopulateRoleDto } from '../dto/find-roles-params.dto';
 import { Injectable } from '@nestjs/common';
-import { sql } from 'kysely';
+import { jsonArrayFrom } from 'kysely/helpers/postgres';
+import { ExpressionBuilder } from 'kysely';
+import { Roles } from 'src/kysely-types';
+
+type RolesExpressionBuilder = ExpressionBuilder<
+  Tables & {
+    r: Roles;
+  },
+  'r'
+>;
 
 @Injectable()
 export class RolesRepository {
   constructor(private readonly db: Database) {}
+
+  private withPermissions(eb: RolesExpressionBuilder) {
+    return jsonArrayFrom(
+      eb
+        .selectFrom('roles_permissions as rp')
+        .leftJoin('permissions as p', 'rp.permission_id', 'p.id')
+        .select(['p.id', 'p.action', 'p.resource'])
+        .whereRef('rp.role_id', '=', 'r.id'),
+    ).as('permissions');
+  }
 
   async create(data: CreateRoleDto): Promise<Role> {
     const dbResponse = await this.db
@@ -26,29 +45,7 @@ export class RolesRepository {
       .selectAll();
 
     if (populate) {
-      query = query
-        .leftJoin('roles_permissions as rp', 'rp.role_id', 'r.id')
-        .leftJoin('permissions as p', 'p.id', 'rp.permission_id')
-        .select(
-          sql<{ id: number; resource: string; action: string }>`  json_agg(
-          json_build_object(
-            'id', p.id,
-            'resource', p.resource,
-            'action', p.action
-          )
-        )`.as('permissions'),
-        )
-        .groupBy([
-          'r.id',
-          'r.created_at',
-          'r.name',
-          'r.updated_at',
-          'rp.role_id',
-          'rp.permission_id',
-          'rp.created_at',
-          'rp.updated_at',
-          'p.id',
-        ]);
+      query = query.select((eb) => [this.withPermissions(eb)]);
     }
 
     const dbResponse = await query.executeTakeFirst();
