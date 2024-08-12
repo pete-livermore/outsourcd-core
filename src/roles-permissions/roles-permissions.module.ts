f03cd7972031ebceb829da3715caf9ec3c54dd9b
Pete Livermore <peterlivermore@gmail.com>
2024-08-12 18:42:46 +1000

    fix: correct import paths for dependencies

--- src/roles-permissions/roles-permissions.module.ts
+++ src/roles-permissions/roles-permissions.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { PermissionsController } from './permissions.controller';
-import { RolesController } from './roles.controller';
+import { PermissionsController } from './controllers/permissions.controller';
+import { RolesController } from './controllers/roles.controller';
 import { PermissionsService } from './services/permissions.service';
 import { RolesService } from './services/roles.service';
 import { RolesRepository } from './repositories/roles.repository';
 import { PermissionsRepository } from './repositories/permissions.repository';
 import { APP_GUARD } from '@nestjs/core';
-import { PermissionsGuard } from './permissions.guard';
+import { PermissionsGuard } from './guards/permissions.guard';
 
 @Module({
   imports: [],
