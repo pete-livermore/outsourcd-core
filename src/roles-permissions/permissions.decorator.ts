f0ce844bb9b360eaf565d81a9c82773e2a17c689
Pete Livermore <peterlivermore@gmail.com>
2024-08-12 18:35:00 +1000

    refactor: remove redundant files

--- src/roles-permissions/permissions.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const PERMISSIONS_KEY = 'permissions';
-export const RequirePermissions = (...permissions: number[]) =>
-  SetMetadata(PERMISSIONS_KEY, permissions);
