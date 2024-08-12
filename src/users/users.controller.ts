c7198c4a7eb53681313802763e032a72f84fffca
Pete Livermore <peterlivermore@gmail.com>
2024-08-12 18:44:09 +1000

    feat: enable authorisation on endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,7 +11,7 @@ import {
   Query,
   Req,
 } from '@nestjs/common';
-import { RequirePermissions } from 'src/roles-permissions/permissions.decorator';
+import { RequirePermissions } from 'src/roles-permissions/decorators/permissions.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -21,13 +21,14 @@ import { PaginationParams } from 'src/common/dto/pagination-params';
 import { Page, PageMetaDto } from 'src/common/dto/page.dto';
 import { Request } from 'src/common/interfaces/request';
 import { ReplaceUserDto } from './dto/replace-user.dto';
+import { RESOURCE_NAME } from './constants/resource-name';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  @RequirePermissions(1)
+  @RequirePermissions({ action: 'read', resource: RESOURCE_NAME })
   async getAll(
     @Query() { filters }: FilterParams = {},
     @Query() { populate }: PopulateParams = {},
@@ -56,6 +57,7 @@ export class UsersController {
   }
 
   @Get(':id')
+  @RequirePermissions({ action: 'read', resource: RESOURCE_NAME })
   async findById(@Param('id', ParseIntPipe) id: number) {
     const user = await this.usersService.findById(id);
 
@@ -63,6 +65,7 @@ export class UsersController {
   }
 
   @Post()
+  @RequirePermissions({ action: 'create', resource: RESOURCE_NAME })
   @HttpCode(201)
   async create(@Body() data: CreateUserDto) {
     const user = await this.usersService.create(data);
@@ -71,6 +74,7 @@ export class UsersController {
   }
 
   @Put(':id')
+  @RequirePermissions({ action: 'update', resource: RESOURCE_NAME })
   async put(
     @Param() params: { id: number },
     @Body() replaceUserDto: ReplaceUserDto,
@@ -79,6 +83,7 @@ export class UsersController {
   }
 
   @Patch(':id')
+  @RequirePermissions({ action: 'update', resource: RESOURCE_NAME })
   async patch(
     @Param() params: { id: number },
     @Body() updateUserDto: UpdateUserDto,
